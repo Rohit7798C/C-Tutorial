876f73bd0c08f6277c88116eb9091dad1f01cf47
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2024-07-30 10:54:25 +0530

    Update Tut31c.c

--- Tutorial31/Tut31c.c
+++ Tutorial31/Tut31c.c
@@ -13,12 +13,12 @@ int sub(int *x, int *y){
 
 int main()
 {
-    int a,b;
+    int a,b,temp1,temp2;
     printf("Enter the value of a and b : ");
     scanf("%d %d",&a, &b);
     printf("The value of a and b are %d and %d\n",a,b);
-    a = sum(&a,&b);
-    b = sub(&a,&b);
-    printf("The value of a and b after sum and difference are %d and %d\n",a,b);
+    temp1 = sum(&a,&b);
+    temp2 = sub(&a,&b);
+    printf("The value of a and b after sum and difference are %d and %d\n",temp1,temp2);
     return 0;
 }
